c9b101e7fe2f78d4938f7aae57d5289e448098b6
Brady <thevillemc@gmail.com>
2021-01-13 17:14:26 +0000

    refactored to separate files

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,31 +1,22 @@
 const request = require('request');
 
-const breed = process.argv.slice(2, 3);
-
-const breedFetcher = (url, done) => {
-  request(`${url}`, (error, response, body) => {
-    if (error) {
-      console.log('Please check your url.');
-      console.log(error);
+const fetchBreedDescription = (breed, done) => {
+  request(`https://api.thecatapi.com/v1/breeds/search/?q=${breed}`, (error, response, body) => {
+    if (error) { //check for error
+      done(error);
       return;
     }
-
-    if (response.statusCode > 299 && response.statusCode < 200) {
-      console.log(error);
+    const data = JSON.parse(body);
+    if (data.length === 0) { // make sure cat exists
+      done('That cat does not exist.');
       return;
     }
-
-    done(body);
-  });
+    if (data[0] !== undefined) { // make sure it has an object
+      done(error, data[0].description);
+    }
+  })
 };
 
-breedFetcher(`https://api.thecasdaatapi.com/v1/breeds/search/?q=${breed}`, (body) => {
-  const data = JSON.parse(body);
-  if (data[0] !== undefined) {
-    const description = data[0].description;
-    console.log(description);
-    return;
-  }
-  console.log('The breed you are looking for is either not a breed or unavailable :3');
-});
+module.exports = fetchBreedDescription;
+
 
---
+++ index.js
@@ -0,0 +1,12 @@
+const fetchBreedDescription = require('./breedFetcher');
+
+const breed = process.argv.slice(2, 3);
+
+fetchBreedDescription(breed, (error, desc) => {
+  if (error) { // error
+    console.log(error);
+    return;
+  } else {
+    console.log(desc);
+  }
+});
